2298dd4d183306caef62fb2a167b85c52951c148
Andrew <Andrewringold@gmail.com>
2022-01-20 20:05:37 -0600

    Added the drive train code
    Still needs controller inputs! uses percent output currently

---
+++ src/main/java/frc/robot/ActuatorMap.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class ActuatorMap {
+    public static final int rMaster = 0;
+    public static final int lMaster = 1;
+    public static final int rSlaveOne = 2;
+    public static final int rSlaveTwo = 3;
+    public static final int lSlaveOne = 4;
+    public static final int lSlaveTwo = 5;
+}
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,8 +1,43 @@
-package frc.robot.subsystems;\
+package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import frc.robot.ActuatorMap;
 
 public class DriveTrain {
+    TalonSRX rMaster = new TalonSRX(ActuatorMap.rMaster);
+    TalonSRX lMaster = new TalonSRX(ActuatorMap.lMaster);
+
+    TalonSRX rSlaveOne = new TalonSRX(ActuatorMap.rSlaveOne);
+    TalonSRX rSlaveTwo = new TalonSRX(ActuatorMap.rSlaveTwo);
+    TalonSRX lSlaveOne = new TalonSRX(ActuatorMap.lSlaveOne);
+    TalonSRX lSlaveTwo = new TalonSRX(ActuatorMap.lSlaveTwo);
+
+    //Drive for the right gearbox
+    public void rDrive(int power) {
+        //set slaves to follow
+        rSlaveOne.follow(rMaster);
+        rSlaveTwo.follow(rMaster);
+
+        //set slaves to be inverted
+        rSlaveOne.setInverted(true);
+        rSlaveTwo.setInverted(true);
+
+        rMaster.set(ControlMode.PercentOutput, power);
+    }
+
+    //Drive for the left gearbox
+    public void lDrive(int power) {
+        //set slaves to follow
+        lSlaveOne.follow(lMaster);
+        lSlaveTwo.follow(lMaster);
+
+        //set slaves to be inverted
+        lSlaveOne.setInverted(true);
+        lSlaveTwo.setInverted(true);
+
+        lMaster.set(ControlMode.PercentOutput, power);
+    }
     
 }
