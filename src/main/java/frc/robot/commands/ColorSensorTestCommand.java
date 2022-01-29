aabdda227d0bd713ff2ce8d0f82cc3da33b3b20c
Savagetechguy <jakeahorner@gmail.com>
2022-01-29 09:46:10 -0600

    make quick stop acummolatss!

--- src/main/java/frc/robot/commands/ColorSensorTestCommand.java
+++ src/main/java/frc/robot/commands/ColorSensorTestCommand.java
@@ -1,7 +1,5 @@
 package frc.robot.commands;
 
-import com.revrobotics.ColorSensorV3;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ColorSensor;
 
--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,11 +1,10 @@
 package frc.robot.commands;
 
-import java.sql.DriverAction;
-
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.utils.Constants;
 import frc.robot.utils.Controls;
+import frc.robot.utils.MathDoer;
 
 public class DriveCommand extends CommandBase {
     
@@ -14,6 +13,7 @@ public class DriveCommand extends CommandBase {
     private double leftY;
     private double rightX;
     private double rightTwist;
+    private double quickStopAcummolatss;
    
 
     public DriveCommand(DriveTrain subsystem) {
@@ -22,38 +22,76 @@ public class DriveCommand extends CommandBase {
     }
 
     @Override
-    public void initialize() {
-       
-
-    }
-
-    @Override
     public void execute() {
         double leftPower = 0;
         double rightPower = 0;
         double turnPower = 0;
+        double overPower = 0;
 
         leftY = Controls.getLeftStickY();
         rightX = Controls.getRightStickX();
         rightTwist = Controls.getRightStickTwist();
 
+
+        //sets the power of the left stick 
         leftPower += leftY;
         rightPower += leftY;
-        //System.out.println("1: " + leftY);
-        //System.out.println(rightX);
+
         //0.1 because of joystick TWIST deadzone are you happy now Jake
         if(Math.abs(rightTwist) > 0.1) {
+
+            //checks if your spinning in place
+            if(Math.abs(leftY) < .2) {
+                //dont worry about it. I have no idea
+                quickStopAcummolatss = .9 * quickStopAcummolatss + .2 * MathDoer.limit(rightTwist, 1);
+            }
+
+            //basiclly a boolean to make it turn in place later in the code
+            overPower = 1.0;
+
+            //sets power of motors to quickTurn
             leftPower -= rightTwist;
             rightPower += rightTwist;
         }
        else{
-           turnPower = Math.abs(leftY) * rightX * Constants.turnSensitivity;
+           //basically a boolean again to adjust turning
+           overPower = 0;
+
+           //adjusts the amount you turn right and left in normal driving mode. Also subtracts the quick stop so we stop quick after 
+           //quick turn
+           turnPower = Math.abs(leftY) * rightX * Constants.turnSensitivity - quickStopAcummolatss;
+
+           //decrease the quick stop after it is used to bring back to 0
+           if (quickStopAcummolatss > 1) {
+                quickStopAcummolatss -= 1;
+            } else if (quickStopAcummolatss < -1) {
+                quickStopAcummolatss += 1;
+            } else {
+                quickStopAcummolatss = 0.0;
+            }
        }
+
+       //makes the motors turn
         rightPower -= turnPower;
         leftPower += turnPower;
-        
-       // System.out.println(leftPower);
 
+        //this is later in the code. corrects overpowering motors when quick turning
+        if (leftPower > 1.0) {
+            rightPower -= overPower * (leftPower - 1.0);
+            leftPower = 1.0;
+        } else if (rightPower > 1.0) {
+            leftPower -= overPower * (rightPower - 1.0);
+            rightPower = 1.0;
+        } else if (leftPower < -1.0) {
+            rightPower += overPower * (-1.0 - leftPower);
+            leftPower = -1.0;
+        } else if (rightPower < -1.0) {
+            leftPower += overPower * (-1.0 - rightPower);
+            rightPower = -1.0;
+        }
+        
+
+        //applies the powers to the motors
         driveTrain.lDrive(leftPower);
         driveTrain.rDrive(rightPower);
 
