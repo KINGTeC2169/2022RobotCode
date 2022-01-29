74fd84a22fa737857ebe90aa6756c066a23621bd
Savagetechguy <jakeahorner@gmail.com>
2022-01-28 19:51:45 -0600

    MATH MANAGER

--- src/main/java/frc/robot/utils/MathDoer.java
+++ src/main/java/frc/robot/utils/MathDoer.java
@@ -1,5 +1,23 @@
 package frc.robot.utils;
 
 public class MathDoer {
-    
+    public static int clamp(int value, int min, int max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static float clamp(float value, float min, float max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static long clamp(long value, long min, long max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static double clamp(double value, double min, double max) {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static double limit(double v, double limit) {
+        return (Math.abs(v) < limit) ? v : limit * (v < 0 ? -1 : 1);
+    }
 }
