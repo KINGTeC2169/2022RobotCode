6c4ed8dd479bebd0412204a3e95a3b10fa1197a1
Ryan Erickson <erirya29@gmail.com>
2022-01-29 20:26:40 -0600

    new constants

--- src/main/java/frc/robot/utils/Constants.java
+++ src/main/java/frc/robot/utils/Constants.java
@@ -2,4 +2,8 @@ package frc.robot.utils;
 
 public final class Constants {
     public static final double turnSensitivity = 1.0;
+
+    //Sensor Units per revolution (Counts per revolution)
+    public static final int TalonFXCPR = 2048;
+    public static final int TalonSRXCPR = 8192;
 }
