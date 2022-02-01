25f9a55123627b002cc83d47b4f1d35e8e50c78e
Ryan Erickson <erirya29@gmail.com>
2022-02-01 15:10:50 -0600

    Limelight left and right

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -6,38 +6,38 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimeLight extends SubsystemBase{
 
-    private static NetworkTable limeLightBack = NetworkTableInstance.getDefault().getTable("limelight-back");
-    private static NetworkTable limeLightFront = NetworkTableInstance.getDefault().getTable("limelight-front");
+    private static NetworkTable limeLightLeft = NetworkTableInstance.getDefault().getTable("limelight-back");
+    private static NetworkTable limeLightRight = NetworkTableInstance.getDefault().getTable("limelight-front");
 
     public double getRightXPercent() {
-        return limeLightFront.getEntry("tx").getDouble(0);
+        return limeLightRight.getEntry("tx").getDouble(0);
     }
 
     public double getRightYPercent() {
-        return limeLightFront.getEntry("ty").getDouble(0);
+        return limeLightRight.getEntry("ty").getDouble(0);
     }
 
     public double getLeftXPercent() {
-        return limeLightBack.getEntry("tx").getDouble(0);
+        return limeLightLeft.getEntry("tx").getDouble(0);
     }
 
     public double getLeftYPercent() {
-        return limeLightBack.getEntry("ty").getDouble(0);
+        return limeLightLeft.getEntry("ty").getDouble(0);
     }
 
     public double getRightDistance() {
-        return (65) / Math.tan(((45+getRightYPercent()) * Math.PI)/180);
+        return (65) / Math.tan(((45 + getRightYPercent()) * Math.PI)/180);
     }
     public double getLeftDistance() {
-        return (65) / Math.tan(((45+getLeftYPercent()) * Math.PI)/180);
+        return (65) / Math.tan(((45 + getLeftYPercent()) * Math.PI)/180);
     }
     
     public void setRightPipeline(int pipelineID) {
-        limeLightFront.getEntry("pipeline").setNumber(pipelineID);
+        limeLightRight.getEntry("pipeline").setNumber(pipelineID);
     }
 
     public void setLeftPipeline(int pipelineID) {
-        limeLightBack.getEntry("pipeline").setNumber(pipelineID);
+        limeLightLeft.getEntry("pipeline").setNumber(pipelineID);
     }
     
 }
