b2e9ebfc0c46568a501ac3ad182a920e4d2a18ea
Ryan Erickson <erirya29@gmail.com>
2022-02-01 11:12:00 -0600

    starting pneunmatics stuff

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -3,16 +3,33 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.ActuatorMap;
 import frc.robot.utils.Constants;
 
 public class Indexer extends SubsystemBase {
     TalonSRX indexer = new TalonSRX(ActuatorMap.indexer);
+    DoubleSolenoid intakePiston = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
+    private static boolean isUp;
 
     public void suckUp(boolean isSucking) {
         if(isSucking)
             indexer.set(ControlMode.PercentOutput, Constants.indexSpeed);
     }
-    //I don't anything about pneunatics still :)
+    //I don't anything about pneumatics still :)
+
+    public void moveIntake() {
+        if(isUp) {
+            intakePiston.set(Value.kForward);
+            isUp = false;
+        }
+        else {
+            intakePiston.set(Value.kReverse);
+            isUp = true;
+        }
+    }
+    //I now know a *little* about pneumatics- as a treat
 }
