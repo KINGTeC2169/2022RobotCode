b15eee5384446eee40e0e332c032887edeb1f450
Savagetechguy <jakeahorner@gmail.com>
2022-02-04 16:52:58 -0600

    Commiting war crimes

--- src/main/java/frc/robot/utils/Constants.java
+++ src/main/java/frc/robot/utils/Constants.java
@@ -4,8 +4,8 @@ public final class Constants {
     public static final double turnSensitivity = 1.0;
 
     //Sensor Units per revolution (Counts per revolution)
-    public static final int TalonFXCPR = 2048;
-    public static final int TalonSRXCPR = 8192;
+    public static final int TalonFXCPR = 8192;
+    public static final int TalonSRXCPR = 2048;
 
     //Speeds for constant motors
     public static final double intakeSpeed = 0.5;
