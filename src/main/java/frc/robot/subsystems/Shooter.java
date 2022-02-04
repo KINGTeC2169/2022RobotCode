a20d2f14bc58abd7f1a7c2b4c390b2b45b9418f7
Ryan Erickson <erirya29@gmail.com>
2022-02-04 16:53:06 -0600

    get RPM

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.ActuatorMap;
+import frc.robot.utils.Constants;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
@@ -11,5 +12,9 @@ public class Shooter extends SubsystemBase {
 
     public void shoot(double power) {
         shooter.set(ControlMode.PercentOutput, power);
+    }
+
+    public double getRPM() {
+        return 600 * shooter.getSelectedSensorVelocity() / Constants.TalonFXCPR;
     }
 }
