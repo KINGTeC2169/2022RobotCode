e5dd5b602053c3eb0b9ee3eae19df34b80110b28
Savagetechguy <jakeahorner@gmail.com>
2022-02-03 18:06:06 -0600

    ballcounter

---
+++ src/main/java/frc/robot/subsystems/BallManager.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+public class BallManager {
+
+    private static String[] ballCount = new String[2];
+
+    public void addBall() {
+        ballCount
+    }
+
+
+    
+}
