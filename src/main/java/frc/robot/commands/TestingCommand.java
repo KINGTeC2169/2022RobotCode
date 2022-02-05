d462b230ffc82368b06fd542b69be03d9500c624
Ryan Erickson <erirya29@gmail.com>
2022-02-05 12:45:47 -0600

    added setRPM test command

--- src/main/java/frc/robot/commands/TestingCommand.java
+++ src/main/java/frc/robot/commands/TestingCommand.java
@@ -1,26 +1,30 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Testing;
 import frc.robot.utils.Controls;
 
 public class TestingCommand extends CommandBase {
 
     private Testing testing;
+    private Shooter shooter;
 
-    public TestingCommand(Testing testing) {
+    public TestingCommand(Testing testing, Shooter shooter) {
         this.testing = testing;
         addRequirements(testing);
+        this.shooter = shooter;
+        addRequirements(shooter);
     }
     
     @Override
     public void execute() {
+        if(Controls.getLeftControllerBumper())
+            shooter.setRPM(500);
         if(Controls.getControllerA())
             testing.extend();
         else if(Controls.getControllerB())
             testing.retract();
-
-
 
 
     }
--- src/main/java/frc/robot/utils/ActuatorMap.java
+++ src/main/java/frc/robot/utils/ActuatorMap.java
@@ -4,14 +4,16 @@ public class ActuatorMap {
     public static final int lJoyStick = 1;
     public static final int rJoyStick = 2;
     public static final int controller = 0;
+
     public static final int lMaster = 0;
     public static final int rMaster = 2;
     public static final int lSlave = 1;
     public static final int rSlave = 3;
+
     public static final int shooter = 6;
-    //I used 26 for temp value, since 0 is like a real thing so...
     public static final int intake = 26;
     public static final int indexer = 26;
     public static final int climber = 26;
+    
     public static final int beamBreak = 26;
 }
