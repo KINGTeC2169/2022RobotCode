283f46892eb08fd44e2c16e5663403a910bcc123
Ryan <erirya29@gmail.com>
2022-02-07 10:06:37 -0600

    made the driveFor() method and added it to auto

--- src/main/java/frc/robot/commands/Autonomous.java
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -10,7 +10,8 @@ public class Autonomous extends CommandBase {
     private DriveTrain driveTrain;
     private Shooter shooter;
 
-    private Timer timer;
+    private Timer timer = new Timer();
+    private int counter;
 
     public Autonomous(DriveTrain driveTrain, Shooter shooter) {
         this.driveTrain = driveTrain;
@@ -26,8 +27,22 @@ public class Autonomous extends CommandBase {
         //Auto plan: shoot then drive backwards
         //Yeah thats it for now
 
-        //Shoot:
+        //Initialization (dont know if we need this or not, i just added it anyways)
+        if(counter == 0) {
 
+            counter++;
+        }
+
+        //Shoot:
+        if(counter == 1) {
+
+            counter++;
+        }
         //Drive:
+        if(counter == 2) {
+            driveTrain.driveFor(24, 0.5);
+            counter++;
+        }
+
     }
 }
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.ActuatorMap;
@@ -10,11 +11,10 @@ import frc.robot.utils.Constants;
 public class DriveTrain extends SubsystemBase {
     TalonSRX rMaster = new TalonSRX(ActuatorMap.rMaster);
     TalonSRX lMaster = new TalonSRX(ActuatorMap.lMaster);
-    
+    //TODO: Find out which motor controllers are Victors
     TalonSRX rSlave = new TalonSRX(ActuatorMap.rSlave);
     TalonSRX lSlave = new TalonSRX(ActuatorMap.lSlave); 
 
-    private double currentPos;
     private double endPos;
 
     //Drive for the right gearbox
@@ -32,7 +32,6 @@ public class DriveTrain extends SubsystemBase {
     //Drive for the left gearbox
     public void lDrive(double power) {
         //set slaves to follow
-        //TODO: why do we set slaves to follow and invert in the methods?
         lSlave.follow(lMaster);
 
 
@@ -45,9 +44,10 @@ public class DriveTrain extends SubsystemBase {
     public void driveFor(double inches, double speed) {
         endPos = Constants.TalonSRXCPR / Constants.wheelCirc;
         lMaster.setSelectedSensorPosition(0);
-        //not sure how i want this to work, auto is weird
-        
-        
+        for(double currentPos = 0.0; currentPos < endPos; currentPos = lMaster.getSelectedSensorPosition()) {
+            rDrive(speed);
+            lDrive(speed);
+        }    
     }
     
 }
