5fc233908ec7eaa3c4986475c5455cfd806ed873
Savagetechguy <jakeahorner@gmail.com>
2022-02-09 16:49:47 -0600

    Robot init bruv

---
+++ src/main/java/frc/robot/commands/RobotInit.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Intake;
+
+
+public class RobotInit extends CommandBase {
+    private Climber climber;
+    private Indexer indexer;
+    private Intake intake;
+
+    public RobotInit(Climber climber, Indexer indexer, Intake intake) {
+        this.climber = climber;
+        addRequirements(climber);
+        this.indexer = indexer;
+        addRequirements(indexer);
+        this.intake = intake;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void execute() {
+        climber.
+    }
+}
