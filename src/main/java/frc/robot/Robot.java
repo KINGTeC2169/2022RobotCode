6edd80edd12762bff0c81f34edde5f178c1385f8
Savagetechguy <jakeahorner@gmail.com>
2022-04-28 09:15:23 -0500

    changed frame rate hope work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -33,8 +33,8 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
     
 
-    CameraServer.startAutomaticCapture(0);
-    CameraServer.startAutomaticCapture(1);
+    CameraServer.startAutomaticCapture(0).setFPS(30);
+    CameraServer.startAutomaticCapture(1).setFPS(30);
   }
 
   /**
